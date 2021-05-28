01199ce1dec066ab1f33c292cdedb159deb53cfb
sqo99 <surifutbol@gmail.com>
2021-05-28 13:35:00 -0500

    Agregué un comentario

--- app/src/main/java/mx/edu/unpa/jessica/MainActivity.java
+++ app/src/main/java/mx/edu/unpa/jessica/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Esto es un comentario de prueba
         Toast.makeText(this, "Hola mundooooooooooooooooo", Toast.LENGTH_SHORT).show();
     }
 }
